7761af4ecb7502b06962203eeba64c1f64568faf
bhargavamakwana <bahrgavamakwana@gmail.com>
2022-08-16 14:15:04 -0400

    checking pipeline with print

--- index-photos.py
+++ index-photos.py
@@ -16,7 +16,7 @@ rekognition = boto3.client("rekognition", region_name = "us-east-1")
 
 def lambda_handler(event, context):
 
-    print("Adding code pipeline")
+    print("Adding code pipeline. Adding more add statements")
     headers = { "Content-Type": "application/json" }    
 
     # TODO implement
